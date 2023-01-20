c7a0fcb8d2eb95ca40109841c0bbb36567924eec
loampo <loampo1@gmail.com>
2023-01-20 14:52:26 +0100

    Scripts Base
    Movimento e AI Base

---
+++ Assets/_Script/HoverControlRay.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverControlRay : MonoBehaviour
+{
+    public float hoverHeight = 3.0f;
+    public float hoverForce = 5.0f;
+    public float forwardAcceleration = 2000.0f;
+    public float backwardAcceleration = 2000.0f;
+    public float turnStrength = 50.0f;
+    public float maxSpeed = 100.0f;
+    public float jumpForce = 5.0f;
+    public LayerMask groundLayers;
+
+    private Rigidbody rb;
+    private Vector3 raycastDirection = Vector3.down;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Calculate the hover force
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, raycastDirection, out hit, hoverHeight, groundLayers))
+        {
+            float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+            Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+            rb.AddForce(appliedHoverForce, ForceMode.Acceleration);
+        }
+
+        // Calculate the forward/backward force
+        float acceleration = 0.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            acceleration = forwardAcceleration;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            acceleration = -backwardAcceleration;
+        }
+        rb.AddForce(transform.forward * acceleration * Time.deltaTime);
+
+        // Limit the speed
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -turnStrength * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turnStrength * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/_Script/Speedometer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour
+{
+    public Image speedometer;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float currentSpeed = rb.velocity.magnitude;
+        float fillAmount = currentSpeed / GameObject.Find("Player").GetComponent<HoverControlRay>().maxSpeed;
+        Debug.Log("Fill Amount: " + fillAmount);
+        speedometer.fillAmount = fillAmount;
+    }
+}
