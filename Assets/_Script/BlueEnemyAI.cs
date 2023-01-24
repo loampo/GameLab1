4769b4e244150be90cc2c39e98c9eda4e6b57088
loampo <loampo1@gmail.com>
2023-01-24 16:30:11 +0100

    Piccoli miglioramenti
    Collider e piccoli miglioramenti IA e Bandiere

---
+++ Assets/_Script/BlueEnemyAI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BlueEnemyAI : MonoBehaviour
+{
+    public List<Transform> blueFlags;
+    public GameObject loseCanvas;
+
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(blueFlags[0].position);
+    }
+
+    private void Update()
+    {
+        if (blueFlags.Count > 0)
+        {
+            agent.SetDestination(blueFlags[0].position);
+        }
+        if (blueFlags.Count == 0)
+        {
+            ShowDefeatScreen();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BlueFlag"))
+        {
+                blueFlags.Remove(other.transform);
+                Destroy(other.gameObject);
+        }
+    }
+
+        void ShowDefeatScreen()
+    {
+        loseCanvas.SetActive(true);
+        Time.timeScale = 0; //Pause the game
+        //You can also set the defeat message on the canvas
+    }
+}
