d577780748b09e3cfa5413a397ca371660e83cb2
Lollo48 <loory4895@gmail.com>
2023-02-02 11:21:11 +0100

    fix all bug
    canvas fix player fix

--- Assets/_Script/Stairs.cs
+++ Assets/_Script/Stairs.cs
@@ -6,7 +6,8 @@ public class Stairs : MonoBehaviour
 {
     public Vector3 offsetRising;
     public BoxCollider colliderStairUp;
-    
+    public BoxCollider[] colls;
+    public Dictionary<Collider, string> collidersMap = new Dictionary<Collider, string>();
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,7 +17,7 @@ public class Stairs : MonoBehaviour
             if (player.transform.position.y - transform.position.y < 0)
             {
                 player.GetComponent<CharacterController>().Move(offsetRising + new Vector3(0, 0, colliderStairUp.bounds.size.z));
-                player.transform.position += offsetRising + new Vector3(0,0,colliderStairUp.bounds.size.z);
+                player.transform.position += offsetRising + new Vector3(0, 0, colliderStairUp.bounds.size.z);
             }
         }
     }
