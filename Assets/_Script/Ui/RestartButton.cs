6ed335b90e88f13003a62b7d26aa27212e2788e2
loampo <loampo1@gmail.com>
2023-02-03 21:34:39 +0100

    Ultima Build
    Fine Modifiche dopo bug fix shield e NavAgent

--- Assets/_Script/Ui/RestartButton.cs
+++ Assets/_Script/Ui/RestartButton.cs
@@ -33,7 +33,11 @@ public class RestartButton : MonoBehaviour
 
     public void Quit()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 
 
